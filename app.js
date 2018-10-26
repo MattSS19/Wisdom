f0d0b1cf069a95f209edd5112ef70bf0aee2e018
Matthew Sylvester <sylvestermatt1@gmail.com>
2018-10-26 16:24:15 +0000

    create book ejs files; modify home.ejs

--- app.js
+++ app.js
@@ -11,6 +11,24 @@ app.get("/book1", function(req, res){
     res.render("book1");
 });
 
+app.get("/book2", function(req, res){
+    res.render("book2");
+});
+
+app.get("/book3", function(req, res){
+    res.render("book3");
+});
+
+app.get("/book4", function(req, res){
+    res.render("book4");
+});
+
+app.get("/book5", function(req, res){
+    res.render("book5");
+});
+
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Wisdom Up!!")
 });
