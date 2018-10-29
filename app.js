06c66d1b504ce6146e23ed69d50a1e776e70567d
Matthew Sylvester <sylvestermatt1@gmail.com>
2018-10-29 23:29:34 +0000

    nearly finish home.ejs. add to main.css for home.ejs. rearrange links for css in header

--- app.js
+++ app.js
@@ -2,30 +2,31 @@ var express = require('express');
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/book1", function(req, res){
-    res.render("book1");
-});
+// app.get("/book1", function(req, res){
+//     res.render("book1");
+// });
 
-app.get("/book2", function(req, res){
-    res.render("book2");
-});
+// app.get("/book2", function(req, res){
+//     res.render("book2");
+// });
 
-app.get("/book3", function(req, res){
-    res.render("book3");
-});
+// app.get("/book3", function(req, res){
+//     res.render("book3");
+// });
 
-app.get("/book4", function(req, res){
-    res.render("book4");
-});
+// app.get("/book4", function(req, res){
+//     res.render("book4");
+// });
 
-app.get("/book5", function(req, res){
-    res.render("book5");
-});
+// app.get("/book5", function(req, res){
+//     res.render("book5");
+// });
 
 
 
